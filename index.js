1df0595f24eca3483f04074e13af582f012f55e8
Aman Mamgain <amanmamgain9@gmail.com>
2022-10-12 20:52:49 +0530

    testing signing messages

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function checkWalletAction() {
         case 'signedTransaction':
             //walletSignTx()
             break;
-        case:
+        default:
             window.walletSignMessage('gg');
             break;
 
