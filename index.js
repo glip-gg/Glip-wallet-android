13ad10f976c0d026addb31f3ea96ca2fb95e65b3
Naman Dwivedi <namandwivedi14@gmail.com>
2022-10-12 19:13:58 +0530

    update tx and message signing logic to use native support

--- index.js
+++ index.js
@@ -1,148 +1,43 @@
 
-var redirectScheme = 'glipwallet://'
+window.androidObj = function AndroidClass() { };
 
-checkWalletAction()
-
-async function checkWalletAction() {
-
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-      });
-
-    redirectScheme = params.redirect_scheme
-    let action = params.action;
-    
-    console.log('wallet action', action)
-
-    switch (action) {
-        case 'login':
-            walletLogin(params.clientId, params.chain, params.network, params.provider)
-            break;
-        case 'logout':
-            walletLogout(params.provider)
-            break;
-        case 'signTx':
-            // decode base64
-            //walletSignTx(atob(params.txData))
-            walletSignTx(params.clientId, params.chainId, params.network, params.txData)
-            break;
-        case 'signMessage':
-            // decode base64
-            walletSignMessage(params.clientId, params.chainId, params.network, params.message)
-            break;
-        case 'sendTx':
-            // decode base64
-            walletSendTx(params.clientId, params.chainId, params.network, params.txData);
-            break;    
-        case 'showWallet': 
-            showWalletUI();
-            break;
-    }
-}
-
-async function walletLogin(clientId, chain, network, provider) {
-    console.log('wallet login', chain, network)
-    setMessage('Logging in..')
-    await window.glipWalletSDK.init({
-        'clientIdentifier': clientId,
-        chain: chain,
-        authNetwork: network
-      }
-    );
-    if (await window.glipWalletSDK.isConnected()) {
-        let walletId = await window.glipWalletSDK.getWalletID()
-        let userInfo = await window.glipWalletSDK.getUserInfo();
-        onWalletLogin(walletId, JSON.stringify(userInfo))
-    } else {
-        window.glipWalletSDK.login(provider, window.location.href)
-    }
-}
-
-async function walletLogout(provider) {
-    console.log('wallet logout', provider)
-    setMessage('Logging out..')
-    await window.glipWalletSDK.logout(provider)
-    onWalletLogout()
-}
-
-async function walletSignTx(clientId, chainId, network, txData) {
-    setMessage(`Signing transaction\n\n${txData}`);
-    await window.glipWalletSDK.init({
-        'clientIdentifier': clientId,
-        chainId: chainId,
-        authNetwork: network
-      }
-    );
-    console.log('gggggggg');
+async function walletSignTx(txData, clientId, chainId) {
+    setMessage(`Signing transaction`);
+    await initialiseWallet(clientId, chainId)
     let signer = await window.glipWalletSDK?.getSigner();
-    let decodedTransactionString = decodeURI(
-        txData);
-    console.log(await signer?.signTransaction(JSON.parse(decodedTransactionString)));
-    //onSignTx(btoa('Placeholder, signed transaction data will be here when implemented'))
+    let signedTx = await signer?.signTransaction(JSON.parse(txData));
+    onWalletActionResult('signTx', signedTx)
 }
 
-async function walletSignMessage(clientId, chainId, network, message) {
+async function walletSignMessage(message, clientId, chainId) {
     console.log('signing message', message)
+    setMessage(`Signing message...`)
+    await initialiseWallet(clientId, chainId)
+    let signer = await window.glipWalletSDK?.getSigner();
+    let signedMessage = await signer?.signMessage(message);
+    onWalletActionResult('signMessage', signedMessage)
+}
+
+
+async function walletSendTx(txData, clientId, chainId) {
+    setMessage(`Sending transaction`);
+    await initialiseWallet(clientId, chainId)
+    let signer = await window.glipWalletSDK?.getSigner();
+    let signedTx = await signer?.sendTransaction(JSON.parse(txData));
+    onWalletActionResult('sendTx', signedTx)
+}
+
+async function initialiseWallet(clientId, chainId) {
     await window.glipWalletSDK.init({
         'clientIdentifier': clientId,
         chainId: chainId,
-        authNetwork: network
+        authNetwork: 'cyan'
     }
     );
-    setMessage(`Signing message\n\n${message}`)
-    let signer = await window.glipWalletSDK?.getSigner();
-    let decodedMessageString = decodeURI(message);
-    decodedMessageString = decodedMessageString.slice(1, -1);
-    await signer?.signMessage(new String(decodedMessageString));
-    setTimeout(() => {
-        //onSignMessage(btoa('Placeholder, signed message will be here when implemented'))
-    }, 2000)
 }
 
-
-async function walletSendTx(clientId, chainId, network, txData) {
-    setMessage(`Sending transaction\n\n${txData}`);
-    await window.glipWalletSDK.init({
-        'clientIdentifier': clientId,
-        chainId: chainId,
-        authNetwork: network
-    });
-    let signer = await window.glipWalletSDK?.getSigner();
-    let decodedTransactionString = decodeURI(txData);
-    console.log('decodedTransactionString', decodedTransactionString);
-    console.log(await signer?.sendTransaction(JSON.parse(decodedTransactionString)));
-    //onSignTx(btoa('Placeholder, signed transaction data will be here when implemented'))
-}
-
-
-function showWalletUI() {
-    window.location.replace('https://glip.gg/wallet-host')
-}
-
-function onWalletLogin(walletId, userInfo) {
-    let deeplink = redirectScheme + `walletConnected?walletId=${walletId}&userInfo=${userInfo}`
-    console.log('wallet connected: opening deeplink', deeplink)
-    window.location = deeplink
-}   
-
-function onWalletLogout() {
-    let deeplink = redirectScheme + `loggedOut`
-    console.log('wallet logged out: opening deeplink', deeplink)
-    window.location = deeplink
-} 
-
-function onSignTx(signedTx) {
-     // base64 encoded signed tx
-     let deeplink = redirectScheme + `walletTxSigned?signedTx=${signedTx}`
-     console.log('wallet signed tx: opening deeplink', deeplink)
-     window.location = deeplink
-}
-
-function onSignMessage(signedMessage) {
-    // base64 encoded signed message
-    let deeplink = redirectScheme + `walletMessageSigned?signedMessage=${signedMessage}`
-    console.log('wallet signed message: opening deeplink', deeplink)
-    window.location = deeplink
+function onWalletActionResult(action, result) {
+    window.androidObj.onWalletActionCallback(action, result);
 }
 
 function setMessage(message) {
